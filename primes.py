f12f223b3e70c6cb48f411cdde590998f397e1ba
Shayna Gail Velasquez <sgavelasquez@gmail.com>
2023-10-19 04:10:07 +0100

    sgav-prime-ans5?

--- primes.py
+++ primes.py
@@ -2,20 +2,27 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def isPrime(num):
-    if num <= 1:
+    if num <= 1: #if number <= 1, not prime (for now?)
         return False
+
+    if num <= 3: #number <= 3, always prime
+        return True
+
+    if num % 2 == 0 or num % 3 == 0: #if divisible by 2 or 3, not prime
+        return False
+
     for i in range(2, int(num**0.5)+1): #start counting from 2; number we are counting to is the sqrt of the number we are checking
-        if num % 1 == 0: #for each num, check if divisible (evenly) by counting number
+        if num % i == 0 or num % (i + 2) == 0: #keep counting while square of counting number is <= number we're checking
             return False #not prime
+        i+=6
     return True #prime
 
 def primes(number_of_primes):
     list = []
-    num = 2 #first prime number
+    num = 2  #start with 2
 
-    while len(list) < number_of_primes: #loop continues
-        if isPrime(num): #for each number, ask if prime
-            list.append(num) #if prime, number gets added to list
-        num += 1 #move on to next number to repeat process
-    
+    while len(list) < number_of_primes: #count until enough numbers in list 
+        if isPrime(num):
+            list.append(num) #if prime, add to list
+        num += 1 #next number
     return list
