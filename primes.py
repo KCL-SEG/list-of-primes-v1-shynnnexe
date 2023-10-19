b1c38106daba6f7a7ba9b8477f734a314858fc66
Shayna Gail Velasquez <sgavelasquez@gmail.com>
2023-10-19 03:52:01 +0100

    ans4

--- primes.py
+++ primes.py
@@ -2,6 +2,8 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def isPrime(num):
+    if num <= 1:
+        return False
     for i in range(2, int(num**0.5)+1): #start counting from 2; number we are counting to is the sqrt of the number we are checking
         if num % 1 == 0: #for each num, check if divisible (evenly) by counting number
             return False #not prime
@@ -15,4 +17,5 @@ def primes(number_of_primes):
         if isPrime(num): #for each number, ask if prime
             list.append(num) #if prime, number gets added to list
         num += 1 #move on to next number to repeat process
+    
     return list
